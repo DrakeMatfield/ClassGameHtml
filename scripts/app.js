a55e99b53613c6602d1e50cde3101ebac2ff88f9
Drake Matfield <drakematfield@yahoo.com>
2018-04-06 21:40:32 +0000

    end of the day work in class

--- scripts/app.js
+++ scripts/app.js
@@ -1,22 +1,27 @@
-var studentProfile = new Profile("drakematfield");
+/////////////////var studentProfile = new Profile("drakematfield");
 
 /**
-* When the JSON body is fully recieved the 
-* the "end" event is triggered and the full body
-* is given to the handler or callback
-**/
-studentProfile.on("end", Display_Results);
+ * When the JSON body is fully recieved the 
+ * the "end" event is triggered and the full body
+ * is given to the handler or callback
+ **/
+//////////////////////studentProfile.on("end", Display_Results);
 
-function Display_Results(readableData) 
-{
+function Display_Results(readableData) {
     console.log(readableData);
 };
 
+
+function Display_error(error) {
+    console.log(error.message);
+};
+
+
 /**
-* If a parsing, network or HTTP error occurs an
-* error object is passed in to the handler or callback
-**/
-studentProfile.on("error", console.error);
+ * If a parsing, network or HTTP error occurs an
+ * error object is passed in to the handler or callback
+ **/
+//////////////////////////////////////studentProfile.on("error", console.error);
 
 //const http = require('http');
 //const hostname = '127.0.0.1';
@@ -33,4 +38,29 @@ studentProfile.on("error", console.error);
 // --- var Profile = require("../scripts/xml_http_profile.js");
 
 
-
+// 1 user enter a username of in on in the treehouse.
+// checked
+
+
+// 2 after clicking the enter the person profile is looked up..
+
+function goto_UserProfile(elementId) {
+
+var username = document.getElementById(elementId).value;
+    // window.location.assign("profile.html")
+    var studentProfile = new Profile(username);
+    studentProfile.on("end", Display_Results);
+    studentProfile.on("error", Display_error);
+
+
+}
+
+// 3 if username exist 
+
+// 4 if person doenst exit error message is shown.
+// 4
+// 5
+// 6
+// 7
+// 8
+// 9
